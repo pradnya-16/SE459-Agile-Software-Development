6256655c1adde03179f9feae284e96a91a8ad9b8
unknown <kpento@depaul.edu>
2025-02-05 19:10:44 -0500

    Very basic room structure...getting something out there for now...

--- rogue/UI/Window.java
+++ rogue/UI/Window.java
@@ -4,20 +4,17 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Window {
+public class Window extends JFrame {
 
-    public void createAndShowGUI() {
-        JFrame frame = new JFrame("Rogue");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JLabel emptyLabel = new JLabel("");
-        emptyLabel.setPreferredSize(new Dimension(800, 500));
-        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
-
-        frame.getContentPane().setBackground(Color.black);
-        frame.pack();
-        frame.setVisible(true);
+    public Window() {
+        setTitle("Rogue");
+      //  getContentPane().setBackground(Color.black);
+        setSize(800,500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+       
     }
+
 
 
 }
